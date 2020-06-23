daedf59899ba0ee005bc076e650a9e60d9f6fb4d
Jason Junio <jjjunio@gmail.com>
2020-06-23 14:21:14 +0000

    project setup and exercise1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,27 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# class Store < ApplicationRecord
+# end
+
+store_burnaby = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true,
+  )
+
+store_richmond = Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  womens_apparel: true,
+  )
+
+store_gastown = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  )
+
+puts Store.count
+
